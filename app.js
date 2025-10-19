2a88f92200521e16f9ca8ac10b3dbd13a4519b45
rsl-design <raressilviulazar@gmail.com>
2025-10-19 18:33:09 +0100

    008   Global Error Handling

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ import express from 'express';
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
-
+import errorMiddleware from './middlewares/error.middleware.js'
 import { PORT } from './config/env.js';
 import connectToDatabase from "./database/mongodb.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
   res.send('Welcome to the subscription tracker API');
