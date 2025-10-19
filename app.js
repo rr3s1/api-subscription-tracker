0e06931b1ecdfdc8b3c3b2a37e2676dfa11116e6
rsl-design <raressilviulazar@gmail.com>
2025-10-19 13:41:23 +0100

    003   Environment Variables & gitignore rule

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Server started");
+import express from 'express';
+
+import { PORT } from './config/env.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the subscription tracker API');
+});
+
+app.listen(PORT, () => {
+console.log(`Subscription tracker API is running on http://localhost:${ PORT }`);
+})
+
+export default app;
